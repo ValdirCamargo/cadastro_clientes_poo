96b13d7b588eef70ef763fa07a601217a3f087b4
Valdir de Camargo <valdir.camargo@yahoo.com.br>
2016-09-15 21:30:27 -0300

    passado para index
    html e  css passado para  index

--- Cliente.php
+++ Cliente.php
@@ -30,7 +30,7 @@ $lista = array(
 
 
 //sort($lista);
-$cliente1 = new Cliente();
+//$cliente1 = new Cliente();
 
 /*for ($i=0;$i<count($lista);$i++ ){
 
@@ -48,58 +48,3 @@ $cliente1 = new Cliente();
  }*/
 ?>
 
-<html>
-
-	<head>
-		<title>Index</title>
-		<style type="text/css">
-    div {
-    width: auto;
-				margin: 30px auto;
-				border: 1px solid #999;
-				padding: 25px;
-				border-radius: 7px;
-				box-shadow: 2px 2px 3px 1px #999;
-			}
-		</style>
-	</head>
-
-	<?php
-	function teste() {
-        ?>
-        <div>
-            <p>
-                <?php sort($lista); ?>
-            </p>
-        </div>
-        <?php
-    }
-	?>
-
-<body>
-<?php
-for ($i=0;$i<count($lista);$i++ ){
-
-    $cliente1->nome=($lista[$i]);
-    $cliente1->cpf=33333333333;
-    $cliente1->endereco="Rua dos loco";
-    $cliente1->telefone=999999999;?>
-<div>
-    <form >
-
-           <?php
-           echo "<br> "."Nome:".$cliente1->nome."<br>"
-                ."CPF: ".$cliente1->cpf."<br>"
-                ."Telefone: ".$cliente1->telefone."<br>"
-                ."Enbdereco: ".$cliente1->endereco."<br>";
-
-        }
-        ?>
-        <br />
-        <input type="submit" value="Ordenar..."<? sort($lista)?> />
-    </form>
-</div>
-
-</body>
-
-</html>
